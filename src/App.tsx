e6089bf7070e92bc4fd92d225e00cf8c7677a82b
Mujtaba Alshakhouri <31612240+Blaiski@users.noreply.github.com>
2022-11-30 10:50:17 +1300

    implemented basic header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import UserCardForm from './components/userCardForm';
 import Menu from './components/menu';
+import { HiMenu} from "react-icons/hi";
 import './App.css';
 
 class App extends React.Component{
@@ -8,6 +9,11 @@ class App extends React.Component{
   render(){
     return(
     <div className="App">
+      <h3 className="App-header">
+          <button>
+            <HiMenu style={{ color: 'grey', fontSize: '50px' }} />
+          </button> User Card Form
+        </h3>
       <UserCardForm userName="Max"/>
       <Menu/>
     </div>);
